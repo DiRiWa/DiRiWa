4c6527cd72664434727d3b8b0e64baa74c05a10b
stef <stefan.marsiske@gmail.com>
2012-04-08 15:33:21 +0200

    [fix] ordered newsitems on /

--- diriwa/context.py
+++ diriwa/context.py
@@ -4,6 +4,6 @@ def menucontext(request):
 	ctx = {}
 	ctx["menu_regions"] = Region.objects.filter(type__in_menu=True)
 	ctx["menu_topics"] = Topic.objects.filter()
-	ctx["recentnews"] = NewsItem.objects.all()[:6]
+	ctx["recentnews"] = NewsItem.objects.order_by('timestamp_submitted').reverse()[:6]
 
 	return ctx
